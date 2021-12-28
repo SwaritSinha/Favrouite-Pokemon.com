12dd19b0a5c7e2cde4a5dfbd97936869789a63ca
SwaritSinha <90327506+SwaritSinha@users.noreply.github.com>
2021-12-28 15:44:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// Make the start button work
+
+const $startButton = document.getElementById("start");
+$startButton.onclick=()=>{
+location.href="./movie.html"
+}
+
+var pokemonimg=document.getElementById("Superior");
+pokemonimg.onclick=()=>{
+    location.href="./secret.html"
+    }
---
+++ gift.js
@@ -0,0 +1,10 @@
+// Make the start button work
+
+const $startButton = document.getElementById("start");
+$startButton.onclick=()=>{
+location.href="Start.html"
+}
+
+document.getElementById('btn').onclick=()=>{
+    document.getElementById('paragraph').style.display="block"
+}
